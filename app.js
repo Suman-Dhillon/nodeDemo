c029a268a03bf75fb89c414329ed269e4415bec4
Suman-Dhillon <119367545+Suman-Dhillon@users.noreply.github.com>
2023-09-14 16:33:11 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let x = 4;
+let y = 3;
+
+let z = x + y;
+console.log(z);
+
+const greeting = "Hello ";
+const name = "Joe";
+
+let msg = greeting + name;
+
+console.log(msg);
+console.log("CPRG306");
